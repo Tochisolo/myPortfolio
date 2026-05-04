d55ef5c80b49b3c18230cad617b0913e0040cf4a
Tochukwu Solomon <kvngfrank96@gmail.com>
2026-05-04 14:43:32 +0100

    feat: add Contact component with EmailJS integration for form submissions

--- src/section/Contact.jsx
+++ src/section/Contact.jsx
@@ -32,8 +32,8 @@ const Contact = () => {
 
 
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        emailjs.init(import.meta.env.VITE_EMAILJS_SERVICE_ID),
+        emailjs.init(import.meta.env.VITE_EMAILJS_TEMPLATE_ID),
         {
           from_name:  form.name,
           from_email: form.email,
